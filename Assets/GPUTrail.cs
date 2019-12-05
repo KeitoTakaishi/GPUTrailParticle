d307b43112d14754cd07a9b7634d206d60ea5ec3
KeitoTakaishi <keitoncis@icloud.com>
2019-12-05 23:28:57 +0900

    GPUTrailParticle

---
+++ Assets/GPUTrail.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUTrail : MonoBehaviour
+{
+
+    #region MeshData
+    [SerializeField] int trailNum;
+    [SerializeField] int trailLength;
+    [SerializeField] float trailWidth;
+    List<Vector3> verticesList;
+    List<Vector3> normalList;
+    List<Vector2> uvList;
+    List<int> indexList;
+    #endregion
+
+
+    #region ComputeShader
+    [SerializeField] ComputeShader cs;
+    const int BLOCK_SIZE = 64;
+    ComputeBuffer positionBuffer;
+    ComputeBuffer velocityBuffer;
+    int kernel;
+    int threadGroupSize;
+    #endregion
+
+
+    #region instancingParams
+    ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5];
+    private int instancingCount;
+    [SerializeField] Mesh srcMesh;
+    [SerializeField] Material instancingMat;
+    #endregion
+
+
+    void Start()
+    {
+        CreateMesh();
+        initInstancingParameter();
+        initBuffer();
+        kernel = cs.FindKernel("update");
+    }
+
+    void Update()
+    {
+
+        cs.SetBuffer(kernel, "positionBuffer", positionBuffer);
+        cs.SetBuffer(kernel, "velocityBuffer", velocityBuffer);
+        cs.SetFloat("dt", Time.deltaTime);
+        cs.SetFloat("time", Time.realtimeSinceStartup);
+        cs.Dispatch(kernel, trailNum, 1, 1);
+
+
+        //Graphics.DrawMeshInstancedIndirect(srcMesh, 0, instancingMat,
+        //new Bounds(Vector3.zero, Vector3.one * 32.0f), argsBuffer, 0, null, UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly, true, 0);
+
+        instancingMat.SetBuffer("positionBuffer", positionBuffer);
+        instancingMat.SetInt("BLOCK_SIZE", BLOCK_SIZE);
+        Graphics.DrawMeshInstancedIndirect(srcMesh, 0, instancingMat,
+        new Bounds(Vector3.zero, Vector3.one * 32.0f), argsBuffer);
+    }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        void CreateMesh()
+    {
+        srcMesh = new Mesh();
+        verticesList = new List<Vector3>();
+        normalList = new List<Vector3>();
+        uvList = new List<Vector2>();
+        indexList = new List<int>();
+
+        for(int i = 0; i < BLOCK_SIZE; i++)
+        {
+            float delta = trailLength / (float)BLOCK_SIZE;
+            var x = (i - BLOCK_SIZE / 2.0f) * delta;
+            Vector3 p1 = new Vector3(x, 0.0f, trailWidth/2.0f);
+            Vector3 p2 = new Vector3(x, 0.0f,-1.0f * trailWidth/2.0f);
+
+            verticesList.Add(p1);
+            verticesList.Add(p2);
+
+            normalList.Add(p1.normalized);
+            normalList.Add(p2.normalized);
+
+
+            uvList.Add(new Vector2( (float)i / (float)(BLOCK_SIZE-1.0), 0.0f));
+            uvList.Add(new Vector2( (float)i / (float)(BLOCK_SIZE-1.0), 1.0f));
+            if(i > 1 && i % 2 == 0)
+            {
+                int i0 = i;
+                int i1 = i + 1;
+                int i2 = i - 1;
+                int i3 = i - 2;
+                indexList.Add(i0);
+                indexList.Add(i1);
+                indexList.Add(i2);
+
+                indexList.Add(i0);
+                indexList.Add(i2);
+                indexList.Add(i3);
+            }
+        }
+
+        srcMesh.vertices = verticesList.ToArray();
+        srcMesh.normals = normalList.ToArray();
+        srcMesh.uv = uvList.ToArray();
+
+        //srcMesh.SetTriangles(indexList.ToArray(), 0);
+        srcMesh.SetIndices(indexList.ToArray(), MeshTopology.Triangles, 0);
+        srcMesh.RecalculateNormals();
+        
+    }
+    //--------------------------------------------------------------------------------------------------------
+    private void initInstancingParameter()
+    {
+        instancingCount = trailNum;
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+
+        args[0] = srcMesh.GetIndexCount(0);
+        args[1] = (uint)instancingCount;
+        args[2] = srcMesh.GetIndexStart(0);
+        args[3] = srcMesh.GetBaseVertex(0);
+        args[4] = 0;
+        argsBuffer.SetData(args);
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    private void initBuffer()
+    {
+        List<Vector3> p = new List<Vector3>();
+        List<Vector3> v = new List<Vector3>();
+
+        positionBuffer = new ComputeBuffer(trailNum * BLOCK_SIZE, sizeof(float) * 3);
+        velocityBuffer = new ComputeBuffer(trailNum * BLOCK_SIZE, sizeof(float) * 3);
+
+
+        for(int i = 0; i < trailNum; i++)
+        {
+            for(int j = 0; j < trailLength; j++)
+            {
+                p.Add(Random.insideUnitSphere * 0.0f);
+                v.Add(Random.insideUnitSphere * 3.0f);
+            }
+        }
+        positionBuffer.SetData(p);
+        velocityBuffer.SetData(v);
+    }
+    //--------------------------------------------------------------------------------------------------------
+    private void OnDisable()
+    {
+        if(argsBuffer != null) argsBuffer.Release();
+        if(positionBuffer != null) positionBuffer.Release();
+        if(velocityBuffer != null) velocityBuffer.Release();
+    }
+}
---
+++ Assets/Scenes/GPUTrailPartilce/Shaders/Visual/Instancing_GPUTrailParticle.shader
@@ -0,0 +1,113 @@
+﻿Shader "Custom/Instancing_GPUTrailParticle"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        Cull Off
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows vertex:vert
+		#pragma multi_compile_instancing
+		#pragma instancing_options procedural:setup
+		#pragma target 3.0
+		#include "../utils.cginc"
+
+		sampler2D _MainTex;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float3 normal : NORMAL;
+			float4 tangent :TANGENT0;
+			float2 texcoord : TEXCOORD0;
+			float2 texcoord1 : TEXCOORD1;
+			float2 texcoord2 : TEXCOORD2;
+			uint instanceID : SV_InstanceID;
+			uint vertexID : SV_VertexID;
+		};
+
+
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+		StructuredBuffer<float3> positionBuffer;
+#endif
+
+		void setup() {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+#endif
+		}
+
+
+		int BLOCK_SIZE;
+		int NumPerFace;
+		void vert(inout appdata v) {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			uint _instanceID = (int)v.instanceID;
+			int _vid = (int)v.vertexID;
+
+			float4 vert = v.vertex;
+			float2 uv = v.texcoord.xy;
+
+
+
+
+
+			int newVertexID = _vid / 2;
+			newVertexID = (int)(fmod((float)(_vid / 2), BLOCK_SIZE)) + (_instanceID)* BLOCK_SIZE;
+
+			if (fmod(_vid, BLOCK_SIZE) != 0.0) {
+				float3 dir = positionBuffer[newVertexID - 1] - positionBuffer[newVertexID];
+				float theta = atan2(dir.y, dir.z);
+				vert = mul(RotXMatrix(theta), vert);
+			}
+			else {
+				float theta = atan2(positionBuffer[newVertexID].y, positionBuffer[newVertexID].z);
+				vert = mul(RotXMatrix(theta), vert);
+			}
+
+			float3 pos = positionBuffer[newVertexID];
+
+			//float4x4 object2world = (float4x4)0.0;
+			//object2world._11_22_33_44 = float4(1.0.xxxx);
+			//object2world._14_24_34 += offSet.xyz;
+			//vert = mul(object2world, vert);
+			vert = mul(TranslateMatrix(pos), vert);
+			v.vertex = vert;
+#endif
+		}
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        UNITY_INSTANCING_BUFFER_START(Props)
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+			float2 uv = IN.uv_MainTex;
+			uv.y *= 0.7;
+			uv.x *= 4.0;
+			uv.x = frac(uv.x);
+            fixed4 c = tex2D (_MainTex, uv) * _Color;
+            o.Albedo = c.rgb;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
