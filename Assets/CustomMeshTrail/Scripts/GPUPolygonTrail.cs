2ec2ca56dc52f2d7d103bb8676b684677132d9c9
keitotakaishi <takaishi78@gmail.com>
2020-04-27 18:32:35 +0900

    update

---
+++ Assets/CustomMeshTrail/Scripts/GPUPolygonTrail.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomMeshTrail
+{
+    public class GPUPolygonTrail : MonoBehaviour
+    {
+        #region Trail-Info
+        [SerializeField] int trailNum;//dispatchの次元数と一致する
+        [SerializeField] int trailPointNum;//何ポイントでトレイルを構成するか
+        [SerializeField] int circleResolution; //断面の解像度
+        [SerializeField] float radius;//断面の半径
+        [SerializeField] float trailLength;
+        List<Vector3> positionList;
+        List<Vector3> normalList;
+        List<Vector2> uvList;
+        List<int> indexList;
+        #endregion
+
+        #region ComputeShaderParameters
+        [SerializeField] ComputeShader cs;
+        ComputeBuffer positionBuffer;
+        ComputeBuffer velocityBuffer;
+        int kernel;
+        uint threadX, threadY, threadZ;
+        #endregion
+
+        #region Instancing-Params
+        ComputeBuffer argsBuffer;
+        private uint[] args = new uint[5];
+        private int instancingCount;
+        Mesh srcMesh;
+        [SerializeField] Material instancingMat;
+        #endregion
+
+        void Start()
+        {
+            if(srcMesh == null)
+            {
+                CreateMesh();
+            }
+            InitInstancingParameter();
+            CreateBuffers();
+            InitBuffers();
+            kernel = cs.FindKernel("Update");
+        }
+
+        void Update()
+        {
+            cs.SetFloat("time", Time.realtimeSinceStartup);
+            cs.SetBuffer(kernel, "positionBuffer", positionBuffer);
+            cs.SetBuffer(kernel, "velocityBuffer", velocityBuffer);
+            cs.Dispatch(kernel, trailNum, 1, 1);
+
+
+            instancingMat.SetFloat("trailNum", trailNum);
+            instancingMat.SetBuffer("positionBuffer", positionBuffer);
+            instancingMat.SetInt("trailPointNum", trailPointNum);
+            instancingMat.SetInt("circleResolution", circleResolution);
+
+            Graphics.DrawMeshInstancedIndirect(srcMesh, 0, instancingMat,
+            new Bounds(Vector3.zero, Vector3.one * 32.0f), argsBuffer);
+        }
+
+        void CreateMesh()
+        {
+            srcMesh = new Mesh();
+            positionList = new List<Vector3>();
+            normalList = new List<Vector3>();
+            uvList = new List<Vector2>();
+            indexList = new List<int>();
+
+            float theta = 2.0f * Mathf.PI / circleResolution;
+            float delta = trailLength / trailPointNum;
+            float deltaUV = 1.0f / (circleResolution - 1);
+            float trainMidPoint = 0.5f * (trailPointNum - 1);
+            for(int i = 0; i < trailPointNum; i++)
+            {
+                float distance = Mathf.Abs(trainMidPoint - i);
+                distance = trainMidPoint - distance;
+                radius = -5.0f * (Mathf.Exp(-0.1f * distance) - 1.05f);
+
+                //radius = Random.Range(radius * 0.5f, radius);
+                for(int j = 0; j < circleResolution; j++)
+                {
+
+                    var p = new Vector3(radius * Mathf.Cos(theta * j), radius * Mathf.Sin(theta * j), i * delta);
+                    positionList.Add(p);
+                    normalList.Add(p);
+                    uvList.Add(new Vector2(1.0f / trailPointNum * i, deltaUV * j));
+                    if(i > 0)
+                    {
+                        int i0 = j + circleResolution * i;
+                        int i1 = (j + 1) % circleResolution + circleResolution * i;
+                        int i2 = j + circleResolution * (i - 1);
+                        int i3 = (j + 1) % circleResolution + circleResolution * (i - 1);
+
+                        
+                        indexList.Add(i0);
+                        indexList.Add(i2);
+                        indexList.Add(i1);
+
+                        indexList.Add(i1);
+                        indexList.Add(i2);
+                        indexList.Add(i3);
+
+                    }
+                }
+            }
+
+            srcMesh.SetVertices(positionList);
+            srcMesh.normals = normalList.ToArray();
+            srcMesh.uv = uvList.ToArray();
+            //srcMesh.SetIndices(indexList.ToArray(), MeshTopology.Triangles, 0);
+            srcMesh.SetIndices(indexList.ToArray(), MeshTopology.LineStrip, 0);
+        }
+        //--------------------------------------------------------------------
+        private void InitInstancingParameter()
+        {
+            instancingCount = trailNum;
+            argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+
+            args[0] = srcMesh.GetIndexCount(0);
+            args[1] = (uint)instancingCount;
+            args[2] = srcMesh.GetIndexStart(0);
+            args[3] = srcMesh.GetBaseVertex(0);
+            args[4] = 0;
+            argsBuffer.SetData(args);
+        }
+        //ComputeBuffer
+        //--------------------------------------------------------------------
+        void CreateBuffers()
+        {
+            positionBuffer = new ComputeBuffer(trailNum * trailPointNum, sizeof(float) * 3);
+            velocityBuffer = new ComputeBuffer(trailNum * trailPointNum, sizeof(float) * 3);
+        }
+        //--------------------------------------------------------------------
+        void InitBuffers()
+        {
+            kernel = cs.FindKernel("Init");
+            cs.SetBuffer(kernel, "positionBuffer", positionBuffer);
+            cs.SetBuffer(kernel, "velocityBuffer", velocityBuffer);
+            cs.Dispatch(kernel, trailNum, 1, 1);
+
+        }
+        //--------------------------------------------------------------------
+        private void OnDisable()
+        {
+            positionBuffer.Release();
+            velocityBuffer.Release();
+        }
+    }
+}
---
+++ Assets/CustomMeshTrail/Shaders/Visual/CustomMeshTrailShader.shader
@@ -0,0 +1,123 @@
+﻿Shader "Custom/CustomMeshTrailShader"
+{
+	Properties
+	{
+		[HDR]
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		Cull off
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows vertex:vert
+		#pragma multi_compile_instancing
+		#pragma instancing_options procedural:setup
+		#pragma target 5.0
+		#include "../utils.cginc"
+		
+		//#include "../NoiseUtils.cginc"
+		//#include "../Noise4d.cginc"
+
+		sampler2D _MainTex;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float3 normal : NORMAL;
+			float4 tangent :TANGENT0;
+			float2 texcoord : TEXCOORD0;
+			float2 texcoord1 : TEXCOORD1;
+			float2 texcoord2 : TEXCOORD2;
+			uint instanceID : SV_InstanceID;
+			uint vertexID : SV_VertexID;
+		};
+
+
+	#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+		StructuredBuffer<float3> positionBuffer;
+	#endif
+
+		void setup() {
+	#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+	#endif
+		}
+
+
+		int trailPointNum;
+		uint circleResolution;
+		float trailNum;
+
+		void vert(inout appdata v) {
+	#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			uint _instanceID = (int)v.instanceID;
+			int _vid = (int)v.vertexID;
+
+			float4 vert = v.vertex;
+			float2 uv = v.texcoord.xy;
+
+
+			//1mesh内でのID
+			int IDinMesh = floor(fmod((float)(_vid / circleResolution), trailPointNum));
+			//インスタンス全体で何番目の中心ポイントなのか
+			int newVertexID = IDinMesh + (_instanceID)* trailPointNum;
+
+
+			float3 forward = float3(0.0, 0.0, -1.0);
+			float theta = dot(forward, positionBuffer[newVertexID]);
+			float axis = cross(float3(normalize(positionBuffer[newVertexID])), forward);
+			//vert = mul(Rodrigues(axis, theta), vert);
+
+
+			float3 pos = positionBuffer[newVertexID];
+			//vert = mul(TranslateMatrix(pos), vert);
+
+			//float r = pulseBuffer[newVertexID].y * pulseBuffer[newVertexID].x;
+
+			float3 normal = normalize(v.vertex.xyz);
+			normal.z = 0.0;
+			//float random = rnd(float2(fmod(_vid, circleResolution), 0.0));
+			//float3 anim = normalize(normal) * r * random;
+			//vert = mul(TranslateMatrix(anim), vert);
+
+
+			//instanceID offSet
+			float x = fmod(_instanceID, 30.0);
+			float y = _instanceID / 30.0;
+			//vert = mul(TranslateMatrix(float3(5.0*(_instanceID*2.0 - trailNum), 5.0, 0.0)), vert);
+			vert = mul(TranslateMatrix(float3(x*10.0, 5.0, y*50.0)), vert);
+			v.vertex = vert;
+	#endif
+		}
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		UNITY_INSTANCING_BUFFER_START(Props)
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf(Input IN, inout SurfaceOutputStandard o)
+		{
+			float2 uv = IN.uv_MainTex;
+			fixed4 c = tex2D(_MainTex, uv) * _Color;
+			c = _Color;
+			o.Albedo = c.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+			FallBack "Diffuse"
+}
